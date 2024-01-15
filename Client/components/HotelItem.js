3414ed477642640b3e0ff15558b0137d84259dc6
Maor Hadad <maorhadad94@gmail.com>
2024-01-15 15:06:57 +0200

    A little design change

--- Client/components/HotelItem.js
+++ Client/components/HotelItem.js
@@ -1,20 +1,36 @@
 import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
-function HotelItem({ hotelName, city,breakfastInfo, dinnerInfo, lobbyBarInfo, spaInfo, 
-  wifiInfo, gymInfo, entertainmentInfo,poolInfo, PoolBarInfo,SynagogueInfo,KeyOnSaturday,checkOutInfo,color, onPress }) {
+function HotelItem({
+  hotelName,
+  city,
+  breakfastInfo,
+  dinnerInfo,
+  lobbyBarInfo,
+  spaInfo,
+  wifiInfo,
+  gymInfo,
+  entertainmentInfo,
+  poolInfo,
+  PoolBarInfo,
+  SynagogueInfo,
+  KeyOnSaturday,
+  checkOutInfo,
+  color,
+  onPress,
+}) {
   return (
     <View style={styles.gridItem}>
-      <Pressable
-        android_ripple={{ color: '#f0f8ff' }}
-        style={({ pressed }) => [
-          styles.button,
-          pressed ? styles.buttonPressed : null,
-        ]}
-        onPress={onPress}
-      >
-        <View style={[styles.innerContainer, { backgroundColor: color }]}>
-          <Text style={styles.title}>{hotelName}</Text>
-          <Text style={styles.city}>{city}</Text>
+    <Pressable
+      android_ripple={{ color: "#f0f8ff", borderless: false }}
+      style={({ pressed }) => [
+      styles.button,
+      pressed ? styles.buttonPressed : null,
+    ]}
+      onPress={onPress}
+    >
+      <View style={[styles.innerContainer, { backgroundColor: color }]}>
+        <Text style={styles.title}>{hotelName}</Text>
+        <Text style={styles.subtitle}>{city}</Text>
         </View>
       </Pressable>
     </View>
@@ -25,40 +41,41 @@ export default HotelItem;
 
 const styles = StyleSheet.create({
   gridItem: {
+    flex: 1,
     margin: 16,
     height: 150,
     borderRadius: 8,
     elevation: 4,
-    backgroundColor: 'white',
-    shadowColor: 'black',
-    shadowOpacity: 0.25,
+    backgroundColor: "white",
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    shadowOpacity: 0.5,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
-    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
   },
   button: {
     flex: 1,
+    borderRadius: 8,
   },
   buttonPressed: {
-    opacity: 0.5,
+    opacity: 0.8,
   },
   innerContainer: {
     flex: 1,
     padding: 16,
     borderRadius: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
+  
   title: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 18,
+    marginBottom: 8,
+    color: "#333", // Adjust text color to match your design
   },
-  city: {
+  subtitle: {
     fontSize: 16,
-    color: '#666',
+    color: "#666",
   },
-  info: {
-    fontSize: 16,
-    color: '#666',
-  }
 });
