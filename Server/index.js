bb03839ad9158420ebb93d412a2b225af279a789
Raphael Benoliel <raphael2gb@gmail.com>
2024-01-26 16:15:56 +0200

    install cors package

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,21 @@
+import StaffRoutes from './routes/StaffRoute';
+import cors from 'cors';
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+const port = process.env.PORT || 8000;
 
+
+app.use(cors());
 // Use body-parser middleware with a specified limit
 app.use(bodyParser.json({ limit: '50mb' }));
 
-const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`app is listening on ${port}`);
 });
 
-
 app.get('/*', async (req, res) => {
     res.send('SHS server');
 });
+
+app.use('/', StaffRoutes);
