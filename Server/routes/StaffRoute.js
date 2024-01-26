4895348ef469b65028f81e2969037c5c47b663af
Raphael Benoliel <raphael2gb@gmail.com>
2024-01-27 01:11:24 +0200

    staff route change

--- Server/routes/StaffRoute.js
+++ Server/routes/StaffRoute.js
@@ -1,9 +1,12 @@
-import express  from "express";
-import { StaffController } from "../controllers/StaffController";
+import express from 'express';
+import { StaffLoginController } from '../controllers/StaffController.js';
 
 const staffRouter = express.Router();
 staffRouter.use(express.json());
+
 staffRouter.post('/staff', async (req, res) => {
-    respond(await StaffController(req.body), res);
+    console.log("staffRouter", req.body);
+    respond(await StaffLoginController(req.body), res);
 });
+
 export default staffRouter;
