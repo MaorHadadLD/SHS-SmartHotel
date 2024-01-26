0c80a55ad5625972f00dd96b33fe4ff09063c609
Raphael Benoliel <raphael2gb@gmail.com>
2024-01-26 16:15:26 +0200

    create the controller for stafff

---
+++ Server/controllers/StaffController.js
@@ -0,0 +1,16 @@
+import { StaffLogin } from "../actions/StaffAction";
+
+export const requestSuccess = (data) => ({success: true, data})
+export const requestFailure = (data) => ({ success: false, data });
+
+
+export const StaffLoginController = async (req, res) => {
+    const { employeeNumber, password } = req.body;
+    const result = await StaffLogin(employeeNumber, password);
+    if (result.success) {
+        res.status(200).json(requestSuccess(result.data))
+    }
+    else {
+        res.status(400).json(requestFailure(result.data))
+    }
+}
