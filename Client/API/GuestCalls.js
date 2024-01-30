27a05f6bbd1d7e2a7ff905f1e6352121485c77d0
Raphael Benoliel <raphael2gb@gmail.com>
2024-01-30 21:41:25 +0200

    chaging api

--- Client/API/GuestCalls.js
+++ Client/API/GuestCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BaseURL = 'http://192.168.1.175:3002/';
+const BaseURL = 'http://192.168.1.161:3002/';
 
 export const sendLoginGuest = async (email, password, selectedHotel) => {
   try {
