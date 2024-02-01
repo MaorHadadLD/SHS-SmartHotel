ef8adc14923180249b6388ede63e35faa587ac55
Raphael Benoliel <raphael2gb@gmail.com>
2024-02-01 17:27:41 +0200

    add post route for avialable rooms

--- Server/routes/StaffRoute.js
+++ Server/routes/StaffRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { StaffLoginController } from '../controllers/StaffController.js';
+import { StaffLoginController, avialableRooms } from '../controllers/StaffController.js';
 import { respond } from './utils.js';
 
 const staffRouter = express.Router();
@@ -7,6 +7,10 @@ staffRouter.use(express.json());
 
 staffRouter.post('/staff', async (req, res) => {
     respond(await StaffLoginController(req.body), res);
+});
+
+staffRouter.post('/availableRooms', async (req, res) => {
+    respond(await avialableRooms(req.body), res);
 });
 
 export default staffRouter;
