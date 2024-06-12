6226c06eddeec5e239256cb71b21352d1d16ad4c
MaorHadadLD <maorhadad94@gmail.com>
2024-06-13 01:12:04 +0300

    updating the room service request screen

--- Client/screens/guest/HotelServices/RoomServiceRequestsScreen.js
+++ Client/screens/guest/HotelServices/RoomServiceRequestsScreen.js
@@ -1,116 +1,172 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Platform } from 'react-native';
-import {Requests} from "../../../data/ClassDpData";
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Platform, ImageBackground, ScrollView } from 'react-native';
+import { Requests } from "../../../data/ClassDpData";
 import { sendPostRequest } from '../../../API/RequestCalls';
 import BackGround from '../../../components/BackGround';
+import CategoryListRoomService from '../../../components/CategoryListRoomService';
+import CartScreen from './CartScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { categoryList, products } from '../../../data/RoomServiceMenu';
 
-function RoomServiceRequestsScreen({ route, navigation}) {
-  const departmentId = 'RoomService'; // Adjust departmentId for room service
-  // const database = getDatabase(firebaseApp);
-  console.log('RoomServiceRequestsScreen route :', route.params);
+function RoomServiceRequestsScreen({ route }) {
+    const [cart, setCart] = useState([]);
 
-  const roomServiceRequests = Requests.filter((reqItem) => {
-    return reqItem.departmentId.includes(departmentId);
-  });
+    const handleAddToCart = (product, quantity) => {
+        if (quantity > 0) {
+            const existingProductIndex = cart.findIndex(item => item.product.id === product.id);
+            if (existingProductIndex > -1) {
+                const newCart = [...cart];
+                newCart[existingProductIndex].quantity += quantity;
+                setCart(newCart);
+            } else {
+                setCart([...cart, { product, quantity }]);
+            }
+        } else {
+            alert('Please enter a valid quantity before adding to cart');
+        }
+    };
 
-  const [customRequest, setCustomRequest] = React.useState('');
-  
-  const handleRequestSubmit = async(request) =>{
-    try {
-      const bodyrequest = { request, type: departmentId, roomNumber: route.params.guestData.roomNumber, selectedHotel: route.params.guestData.selectedHotel };
-      const response = await sendPostRequest(bodyrequest);
-      if (response.success) {
-        alert('Request submitted successfully');
-        navigation.navigate('ClientMainMenu', { selectedHotel: route.params.selectedHotel, guestData: route.params.guestData });
-      }
-    } catch (error) {
-      console.error('Room Service request error:', error.message);
-    }
-  }
+    const handleSendOrder = async () => {
+        try {
+            if (cart.length > 0) {
+                const order = {
+                    cart: cart.map(item => ({
+                        productId: item.product.id,
+                        quantity: item.quantity,
+                    })),
+                    type: 'RoomService',
+                    roomNumber: route.params.guestData.roomNumber,
+                    selectedHotel: route.params.guestData.selectedHotel,
+                };
+                const response = await sendPostRequest(order); // Replace with your API request function
+                if (response.success) {
+                    alert('Order submitted successfully');
+                    setCart([]);
+                }
+            } else {
+                alert('Please add items to the cart before sending the order');
+            }
+        } catch (error) {
+            console.error('Error sending order:', error.message);
+        }
+    };
 
-  function renderPredefinedRequestButton(request) {
+    const handleRemoveFromCart = (productId) => {
+        setCart(cart.filter(item => item.product.id !== productId));
+    };
+
+    const handleUpdateQuantity = (productId, newQuantity) => {
+        setCart(cart.map(item =>
+            item.product.id === productId ? { ...item, quantity: newQuantity } : item
+        ));
+    };
+
+    const Tab = createBottomTabNavigator();
+
     return (
-      <TouchableOpacity
-        key={request.id}
-        style={styles.requestButton}
-        onPress={() => handleRequestSubmit(request.requestNotice)}
-      >
-        <Text style={styles.buttonText}>{request.requestNotice}</Text>
-      </TouchableOpacity>
+        <Tab.Navigator>
+            <Tab.Screen
+                name="RoomServiceMenu"
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="restaurant" color={color} size={size} />
+                    ),
+                }}
+            >
+                {() => (
+                    <View style={styles.container}>
+                        <ImageBackground source={require('../../../assets/room_service.jpg')} style={styles.backgroundImage}>
+                            <View style={styles.overlay}>
+                                <ScrollView>
+                                    <CategoryListRoomService
+                                        categoryList={categoryList}
+                                        products={products}
+                                        onAddToCart={handleAddToCart}
+                                    />
+                                </ScrollView>
+                            </View>
+                        </ImageBackground>
+                    </View>
+                )}
+            </Tab.Screen>
+            <Tab.Screen
+                name="Cart"
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="cart" color={color} size={size} />
+                    ),
+                }}
+            >
+                {() => (
+                    <CartScreen
+                        cart={cart}
+                        handleRemoveFromCart={handleRemoveFromCart}
+                        handleUpdateQuantity={handleUpdateQuantity}
+                        handleSendOrder={handleSendOrder}
+                    />
+                )}
+            </Tab.Screen>
+        </Tab.Navigator>
     );
-  }
-
-  return (
-    <BackGround>
-    <View style={styles.container}>
-      <Text style={styles.title}>Choose a predefined request:</Text>
-      <View style={styles.buttonContainer}>
-        {roomServiceRequests.map((request) =>
-            renderPredefinedRequestButton(request)
-        )}
-      </View>
-
-      <Text style={styles.title}>Or enter your own request:</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Type your request here..."
-        onChangeText={(text) => setCustomRequest(text)}
-        value={customRequest}
-      />
-
-      <TouchableOpacity
-        style={styles.submitButton}
-        onPress={() => handleRequestSubmit(customRequest)}
-        disabled={roomServiceRequests.length === 0}
-      >
-        <Text style={styles.buttonText}>Submit Request</Text>
-      </TouchableOpacity>
-    </View>
-    </BackGround>
-  );
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      padding: 16,
-      marginTop: Platform.OS === 'ios' ? 40 : 0, // Adjust marginTop for iOS status bar
+        flex: 1,
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    overlay: {
+        flex: 1,
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+        paddingHorizontal: 10,
+    },
+    customRequestContainer: {
+        padding: 16,
+        marginTop: Platform.OS === 'ios' ? 40 : 0, // Adjust marginTop for iOS status bar
     },
     title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginTop: 10,
-      color: 'white'
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 10,
+        color: '#333',
+        textAlign: 'center',
     },
     buttonContainer: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      marginTop: 10,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginTop: 10,
+        justifyContent: 'center',
     },
     requestButton: {
-      backgroundColor: '#3498db',
-      borderRadius: 5,
-      padding: 10,
-      margin: 5,
+        backgroundColor: '#3498db',
+        borderRadius: 5,
+        padding: 10,
+        margin: 5,
+        minWidth: '40%',
     },
     buttonText: {
-      color: '#fff',
-      textAlign: 'center',
+        color: '#fff',
+        textAlign: 'center',
     },
     input: {
-      height: 40,
-      borderColor: 'gray',
-      borderWidth: 1,
-      marginTop: 10,
-      marginBottom: 10,
-      padding: 8,
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 10,
+        marginBottom: 10,
+        padding: 8,
+        borderRadius: 5,
     },
     submitButton: {
-      backgroundColor: '#27ae60',
-      borderRadius: 5,
-      padding: 10,
-      marginTop: 10,
-      opacity: 0.6, // Adjust the opacity to visually indicate that the button is disabled
+        backgroundColor: '#27ae60',
+        borderRadius: 5,
+        padding: 10,
+        marginTop: 10,
+        opacity: 0.9,
     },
-  });
+});
 export default RoomServiceRequestsScreen;
