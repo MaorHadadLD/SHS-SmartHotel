d5d1ec92038c3ead35cf3b2a788f7a5e6a6bf2b0
MaorHadadLD <maorhadad94@gmail.com>
2024-06-13 01:10:43 +0300

    adding data for the room service request

---
+++ Client/data/RoomServiceMenu.js
@@ -0,0 +1,50 @@
+export const categoryList = [
+    { id: 1, name: 'Food' },
+    { id: 2, name: 'Hot Drinks' },
+    { id: 3, name: 'Soft Drinks' },
+    { id: 4, name: 'Wines' },
+    { id: 5, name: 'Treats' },
+    { id: 6, name: 'Desserts' },
+];
+
+export const products = [
+    // Food
+    { id: 1, category: 'Food', name: 'Burger', price: 15 },
+    { id: 2, category: 'Food', name: 'Pizza', price: 20 },
+    { id: 3, category: 'Food', name: 'Caesar Salad', price: 12 },
+    { id: 4, category: 'Food', name: 'Pasta', price: 18 },
+    { id: 5, category: 'Food', name: 'Grilled Chicken', price: 25 },
+    
+    // Hot Drinks
+    { id: 6, category: 'Hot Drinks', name: 'Coffee', price: 5 },
+    { id: 7, category: 'Hot Drinks', name: 'Tea', price: 4 },
+    { id: 8, category: 'Hot Drinks', name: 'Hot Chocolate', price: 6 },
+    { id: 9, category: 'Hot Drinks', name: 'Cappuccino', price: 5 },
+    { id: 10, category: 'Hot Drinks', name: 'Latte', price: 5 },
+    
+    // Soft Drinks
+    { id: 11, category: 'Soft Drinks', name: 'water', price: 3 },
+    { id: 12, category: 'Soft Drinks', name: 'Lemonade', price: 4 },
+    { id: 13, category: 'Soft Drinks', name: 'Iced Tea', price: 4 },
+    { id: 14, category: 'Soft Drinks', name: 'Sparkling Water', price: 3 },
+    { id: 15, category: 'Soft Drinks', name: 'Cola', price: 3 },
+    
+    // Wines
+    { id: 16, category: 'Wines', name: 'Red Wine', price: 30 },
+    { id: 17, category: 'Wines', name: 'White Wine', price: 30 },
+    { id: 18, category: 'Wines', name: 'Rosé Wine', price: 30 },
+    { id: 19, category: 'Wines', name: 'Champagne', price: 50 },
+    { id: 20, category: 'Wines', name: 'Prosecco', price: 25 },
+    
+    // Treats
+    { id: 21, category: 'Treats', name: 'Cookie', price: 2 },
+    { id: 22, category: 'Treats', name: 'Brownie', price: 3 },
+    { id: 23, category: 'Treats', name: 'Cupcake', price: 4 },
+    { id: 24, category: 'Treats', name: 'Muffin', price: 3 },
+    { id: 25, category: 'Treats', name: 'Macaroon', price: 5 },
+    
+    // Desserts
+    { id: 26, category: 'Desserts', name: 'Chocolate Cake', price: 8 },
+    { id: 27, category: 'Desserts', name: 'Ice Cream', price: 6 },
+    { id: 28, category: 'Desserts', name: 'Fruit Salad', price: 7 },
+];
