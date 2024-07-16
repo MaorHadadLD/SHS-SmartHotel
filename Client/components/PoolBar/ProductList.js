582e69f5edca2b39104df320139fd11124084ae5
Raphael Benoliel <raphael2gb@gmail.com>
2024-07-16 15:40:59 +0300

    adding image to poolbar menu and roomservices menu.

--- Client/components/PoolBar/ProductList.js
+++ Client/components/PoolBar/ProductList.js
@@ -1,71 +1,71 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
-import { products } from '../../data/PoolBarMenu';
+// import React, { useState } from 'react';
+// import { View, StyleSheet, Text, ScrollView, TouchableOpacity, Image } from 'react-native';
+// import { products } from '../../data/PoolBarMenu';
 
-const ProductList = ({ route, navigation }) => {
-    const { category } = route.params;
+// const ProductList = ({ route, navigation }) => {
+//     const { category } = route.params;
 
-    const categoryProducts = products.filter(product => product.category === category.name);
+//     const categoryProducts = products.filter(product => product.category === category.name);
 
-    return (
-        <ScrollView style={styles.productListContainer}>
-            {categoryProducts.map(product => (
-                <View key={product.id} style={styles.productContainer}>
-                    <Image source={{ uri: product.image }} style={styles.productImage} />
-                    <View style={styles.productDetails}>
-                        <Text style={styles.productName}>{product.name}</Text>
-                        <Text style={styles.productPrice}>${product.price.toFixed(2)}</Text>
-                        <TouchableOpacity
-                            style={styles.addButton}
-                            onPress={() => handleAddToCart(product, 1)}
-                        >
-                            <Text style={styles.addButtonText}>Add to Cart</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            ))}
-        </ScrollView>
-    );
-};
+//     return (
+//         <ScrollView style={styles.productListContainer}>
+//             {categoryProducts.map(product => (
+//                 <View key={product.id} style={styles.productContainer}>
+//                     <Image source={product.image} style={styles.productImage} />
+//                     <View style={styles.productDetails}>
+//                         <Text style={styles.productName}>{product.name}</Text>
+//                         <Text style={styles.productPrice}>${product.price.toFixed(2)}</Text>
+//                         <TouchableOpacity
+//                             style={styles.addButton}
+//                             onPress={() => handleAddToCart(product, 1)}
+//                         >
+//                             <Text style={styles.addButtonText}>Add to Cart</Text>
+//                         </TouchableOpacity>
+//                     </View>
+//                 </View>
+//             ))}
+//         </ScrollView>
+//     );
+// };
 
-const styles = StyleSheet.create({
-    productListContainer: {
-        flex: 1,
-        backgroundColor: '#f8f8f8',
-    },
-    productContainer: {
-        flexDirection: 'row',
-        marginBottom: 10,
-    },
-    productImage: {
-        width: 80,
-        height: 80,
-        borderRadius: 8,
-        marginRight: 10,
-    },
-    productDetails: {
-        flex: 1,
-    },
-    productName: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-    productPrice: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: '#333',
-    },
-    addButton: {
-        marginTop: 5,
-        backgroundColor: '#FF6B3C',
-        padding: 10,
-        borderRadius: 8,
-        alignItems: 'center',
-    },
-    addButtonText: {
-        color: '#fff',
-        fontSize: 16,
-    },
-});
+// const styles = StyleSheet.create({
+//     productListContainer: {
+//         flex: 1,
+//         backgroundColor: '#f8f8f8',
+//     },
+//     productContainer: {
+//         flexDirection: 'row',
+//         marginBottom: 10,
+//     },
+//     productImage: {
+//         width: 80,
+//         height: 80,
+//         borderRadius: 8,
+//         marginRight: 10,
+//     },
+//     productDetails: {
+//         flex: 1,
+//     },
+//     productName: {
+//         fontSize: 18,
+//         fontWeight: 'bold',
+//     },
+//     productPrice: {
+//         fontSize: 16,
+//         fontWeight: 'bold',
+//         color: '#333',
+//     },
+//     addButton: {
+//         marginTop: 5,
+//         backgroundColor: '#FF6B3C',
+//         padding: 10,
+//         borderRadius: 8,
+//         alignItems: 'center',
+//     },
+//     addButtonText: {
+//         color: '#fff',
+//         fontSize: 16,
+//     },
+// });
 
-export default ProductList;
+// export default ProductList;
--- Client/data/RoomServiceMenu.js
+++ Client/data/RoomServiceMenu.js
@@ -9,42 +9,42 @@ export const categoryList = [
 
 export const products = [
     // Food
-    { id: 1, category: 'Food', name: 'Burger', price: 15 },
-    { id: 2, category: 'Food', name: 'Pizza', price: 20 },
-    { id: 3, category: 'Food', name: 'Caesar Salad', price: 12 },
-    { id: 4, category: 'Food', name: 'Pasta', price: 18 },
-    { id: 5, category: 'Food', name: 'Grilled Chicken', price: 25 },
+    { id: 1, category: 'Food', name: 'Burger', price: 15 , img: require('../assets/RoomServices/burger.png')},
+    { id: 2, category: 'Food', name: 'Pizza', price: 20 , img: require('../assets/RoomServices/pizza.png')},
+    { id: 3, category: 'Food', name: 'Caesar Salad', price: 12, img: require('../assets/RoomServices/ceaser_salad.png')},
+    { id: 4, category: 'Food', name: 'Pasta', price: 18 , img: require('../assets/RoomServices/pasta.png')},
+    { id: 5, category: 'Food', name: 'Chicken Breast', price: 25, img: require('../assets/RoomServices/chicken_breast.png')},
     
     // Hot Drinks
-    { id: 6, category: 'Hot Drinks', name: 'Coffee', price: 5 },
-    { id: 7, category: 'Hot Drinks', name: 'Tea', price: 4 },
-    { id: 8, category: 'Hot Drinks', name: 'Hot Chocolate', price: 6 },
-    { id: 9, category: 'Hot Drinks', name: 'Cappuccino', price: 5 },
-    { id: 10, category: 'Hot Drinks', name: 'Latte', price: 5 },
+    { id: 6, category: 'Hot Drinks', name: 'Coffee', price: 5, img: require('../assets/RoomServices/coffee.png')},
+    { id: 7, category: 'Hot Drinks', name: 'Tea', price: 4, img: require('../assets/RoomServices/tea.png')},
+    { id: 8, category: 'Hot Drinks', name: 'Hot Chocolate', price: 6, img: require('../assets/RoomServices/choco.png')},
+    { id: 9, category: 'Hot Drinks', name: 'Cappuccino', price: 5, img: require('../assets/RoomServices/cappuccino.png')},
+    { id: 10, category: 'Hot Drinks', name: 'Latte', price: 5, img: require('../assets/RoomServices/latte.png')},
     
     // Soft Drinks
-    { id: 11, category: 'Soft Drinks', name: 'water', price: 3 },
-    { id: 12, category: 'Soft Drinks', name: 'Lemonade', price: 4 },
-    { id: 13, category: 'Soft Drinks', name: 'Iced Tea', price: 4 },
-    { id: 14, category: 'Soft Drinks', name: 'Sparkling Water', price: 3 },
-    { id: 15, category: 'Soft Drinks', name: 'Cola', price: 3 },
+    { id: 11, category: 'Soft Drinks', name: 'Water', price: 3, img: require('../assets/RoomServices/water.png')},
+    { id: 12, category: 'Soft Drinks', name: 'Lemonade', price: 4 , img: require('../assets/RoomServices/lemonade.png')},
+    { id: 13, category: 'Soft Drinks', name: 'Nestea', price: 4, img: require('../assets/RoomServices/icetea.png')},
+    { id: 14, category: 'Soft Drinks', name: 'Sparkling Water', price: 3, img: require('../assets/RoomServices/sparkwater.png')},
+    { id: 15, category: 'Soft Drinks', name: 'Coke', price: 3 , img: require('../assets/RoomServices/coke.png')},
     
     // Wines
-    { id: 16, category: 'Wines', name: 'Red Wine', price: 30 },
-    { id: 17, category: 'Wines', name: 'White Wine', price: 30 },
-    { id: 18, category: 'Wines', name: 'Rosé Wine', price: 30 },
-    { id: 19, category: 'Wines', name: 'Champagne', price: 50 },
-    { id: 20, category: 'Wines', name: 'Prosecco', price: 25 },
+    { id: 16, category: 'Wines', name: 'Red Wine', price: 30, img: require('../assets/RoomServices/redwine.png')},
+    { id: 17, category: 'Wines', name: 'White Wine', price: 30, img: require('../assets/RoomServices/whitewine.png')},
+    { id: 18, category: 'Wines', name: 'Rosé Wine', price: 30 , img: require('../assets/RoomServices/rose.png')},
+    { id: 19, category: 'Wines', name: 'Champagne', price: 50, img: require('../assets/RoomServices/champagne.png')},
+    { id: 20, category: 'Wines', name: 'Prosecco', price: 25, img: require('../assets/RoomServices/prosecco.png')},
     
     // Treats
-    { id: 21, category: 'Treats', name: 'Cookie', price: 2 },
-    { id: 22, category: 'Treats', name: 'Brownie', price: 3 },
-    { id: 23, category: 'Treats', name: 'Cupcake', price: 4 },
-    { id: 24, category: 'Treats', name: 'Muffin', price: 3 },
-    { id: 25, category: 'Treats', name: 'Macaroon', price: 5 },
+    { id: 21, category: 'Treats', name: 'Cookie', price: 2, img: require('../assets/RoomServices/cookie.png')},
+    { id: 22, category: 'Treats', name: 'Brownie', price: 3, img: require('../assets/RoomServices/brownie.png')},
+    { id: 23, category: 'Treats', name: 'Cupcake', price: 4, img: require('../assets/RoomServices/cupcake.png')},
+    { id: 24, category: 'Treats', name: 'Muffin', price: 3, img: require('../assets/RoomServices/muffin.png')},
+    { id: 25, category: 'Treats', name: 'Macaroon', price: 5, img: require('../assets/RoomServices/macaroon.png')},
     
     // Desserts
-    { id: 26, category: 'Desserts', name: 'Chocolate Cake', price: 8 },
-    { id: 27, category: 'Desserts', name: 'Ice Cream', price: 6 },
-    { id: 28, category: 'Desserts', name: 'Fruit Salad', price: 7 },
+    { id: 26, category: 'Desserts', name: 'Chocolate Cake', price: 8, img: require('../assets/RoomServices/chocolate_cake.png')},
+    { id: 27, category: 'Desserts', name: 'Ice Cream', price: 6, img: require('../assets/RoomServices/icecream.png')},
+    { id: 28, category: 'Desserts', name: 'Fruit Salad', price: 7, img: require('../assets/RoomServices/fruitsalad.png')},
 ];
