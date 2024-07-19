4b61d5959f3a54f2d8c5fc522b7e0c8dfb5a69fa
MaorHadadLD <maorhadad94@gmail.com>
2024-07-19 20:23:54 +0300

    add asp treatments data

---
+++ Client/data/spaTreatments.js
@@ -0,0 +1,10 @@
+// src/data/spaTreatments.js
+export const spaTreatments = [
+    { type: 'Swedish Massage', price: '100/230' },
+    { type: 'Deep Tissue Massage', price: '120/250' },
+    { type: 'Aromatherapy Massage', price: '110/240' },
+    { type: 'Hot Stone Massage', price: '130/270' },
+    { type: 'Shiatsu Massage', price: '115/240' },
+    { type: 'Thai Massage', price: '125/260' },
+    { type: 'Mud Wrap', price: '100/210' },
+];
